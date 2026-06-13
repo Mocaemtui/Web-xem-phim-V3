0d80f3375d91390ef6fc1ededac032a08452976c
Mocaemtui <haodangpc05@gmail.com>
2026-06-13 22:34:01 +0700

    chore: clean up test scripts

--- test-nguonc-query.js
+++
@@ -1,7 +0,0 @@
-const NGUONC_BASE_URL = "https://phim.nguonc.com/api";
-async function testNguonC() {
-  const detailRes = await fetch(`${NGUONC_BASE_URL}/film/ngoi-truong-xac-song?_v=1`);
-  const detailData = await detailRes.json();
-  console.log("NguonC with query param worked:", !!detailData.movie);
-}
-testNguonC();
--- test-nguonc.js
+++
@@ -1,7 +0,0 @@
-const NGUONC_BASE_URL = "https://phim.nguonc.com/api";
-async function testNguonC() {
-  const detailRes = await fetch(`${NGUONC_BASE_URL}/film/ngoi-truong-xac-song`);
-  const detailData = await detailRes.json();
-  console.log("NguonC episodes:", JSON.stringify(detailData.movie.episodes, null, 2));
-}
-testNguonC();
